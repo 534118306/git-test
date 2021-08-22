529f0f0ff11ed84750eabd2dafa2b0ef98ac0f8f
王明坤 <534118306@qq.com>
2021-08-22 09:29:25 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Young
+ * @create 2021/8/22-9:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
